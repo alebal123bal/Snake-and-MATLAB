2e232a80a84d3197d928f12daee6ea450a0f7577
Alessandro Balzan <balzanalessandro2001@gmail.com>
2022-09-23 12:40:22 +0200

    Connect

--- snake_player.py
+++ snake_player.py
@@ -1,5 +1,16 @@
-import matlab.engine
+#import matlab.engine
+import pygame
 
-eng = matlab.engine.connect_matlab()
+#eng = matlab.engine.connect_matlab()
 
-print(eng.sqrt(4.0))
+SQUARE_SIZE = 50
+GREEN = (0, 255, 0)
+
+pygame.init()
+dis = pygame.display.set_mode((10*SQUARE_SIZE, 10*SQUARE_SIZE))
+clock = pygame.time.Clock()
+
+pygame.draw.rect(dis, GREEN, [0, 0, 1*SQUARE_SIZE, 1*SQUARE_SIZE])
+pygame.display.update()
+
+clock.tick(1)
